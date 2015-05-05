c6638437d29a501bbd98fc3636ed3769f735d74a
Carson <carsondron@gmail.com>
2015-05-06 04:52:40 +1000

    Adding stored proc.

---
+++ Stored Procedures/dbo.ChangeUsername.sql
@@ -0,0 +1,23 @@
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_NULLS ON
+GO
+-- =============================================
+-- Author:		Carson Dron
+-- Create date: 
+-- Description:	
+-- =============================================
+CREATE PROCEDURE [dbo].[ChangeUsername] 
+	-- Add the parameters for the stored procedure here
+	@oldname NVARCHAR(256),
+	@newname nvarchar(256)
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+	UPDATE Users SET UserName = @newname WHERE UserName = @oldname
+END
+GO
